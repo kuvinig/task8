5d43ceb3e51c2d60754776056bbfe8e934607fc0
kuvinig <kuvi_work@outlook.com>
2024-12-20 15:37:41 +0700

    first iteration

---
+++ task.py
@@ -0,0 +1,45 @@
+class InputData:
+    def __init__(self):
+        pass
+
+    def manual_input(self):
+        pass
+
+    def random_input(self):
+        pass
+
+
+class Algorithm:
+    def __init__(self):
+        pass
+
+    def execute(self, matrix):
+        pass
+
+
+class ResultOutput:
+    def __init__(self):
+        pass
+
+    def display(self, sorted_by_rows, sorted_by_columns):
+        pass
+
+
+class Application:
+    def __init__(self):
+        pass
+
+    def input_data(self):
+        pass
+
+    def run_algorithm(self):
+        pass
+
+    def show_results(self):
+        pass
+
+    def exit_program(self):
+        pass
+
+    def run(self):
+        pass
